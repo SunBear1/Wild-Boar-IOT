05ffdea9db68daacdb70b2622c9d34d6b15344a0
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2022-10-19 20:10:08 +0200

    new typescript version of the Vue app

---
+++ src/new_web_ui/src/components/TodoList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        {{ TITLE }}
+        <button @click="AddToCounter"> {{ clickCount }} </button>
+        <button @click="ReverseName">Reverse Task Name</button>
+        <span> {{ task.title }} </span>
+        <div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive } from 'vue';
+import type { Ref } from 'vue';
+interface Task {
+    title: String,
+    done: Boolean,
+}
+const TITLE = 'Todo list';
+const clickCount: Ref<number> = ref(0);
+const task: Task = reactive({
+    title: '1st Task',
+    done: false,
+});
+
+const AddToCounter = () => {
+    clickCount.value += 1;
+}
+const ReverseName = () => {
+    task.title = task.title.split("").reverse().join("");
+}
+</script>
+
+<style scoped>
+
+</style>
