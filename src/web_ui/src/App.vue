21150d298217cc2b7fc8133783a36b2ebcdad4b3
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2022-10-19 20:19:44 +0200

    fixing git mistakes

---
+++ src/web_ui/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <TodoList />
+    </div>
+</template>
+
+<script setup lang="ts">
+import TodoList from './components/TodoList.vue';
+</script>
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+    }
+</style>
