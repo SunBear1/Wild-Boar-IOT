9b0c6f52c09ea57b79dd28c2edb133d64bbe7496
Jakub Sachajko <kuba.sachajko@gmail.com>
2022-10-20 13:19:18 +0200

    Update dockerfile and sh script

--- src/rabbitmq/initialize_docker.sh
+++ src/rabbitmq/initialize_docker.sh
@@ -1,5 +1,5 @@
 
-docker build -t rabbit_image . ;
+docker build -t quemessagingsystem/rabbit_image . ;
 echo 'INFO: Image rabbit_image created!'
-docker run -d -it -p 15672:15672 --name=rabbit_container rabbit_image;
+docker run -d -it -p 15672:15672 --name=rabbit_container quemessagingsystem/rabbit_image;
 echo 'INFO: Started container'
