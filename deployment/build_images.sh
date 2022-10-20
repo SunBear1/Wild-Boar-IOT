1c003dce8ab85d6c1a6ff077368dc517e235178e
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2022-10-20 22:03:29 +0200

    deployment component

---
+++ deployment/build_images.sh
@@ -0,0 +1,11 @@
+echo "Building web_ui image..."
+cd ../src/web_ui; docker build -t $WEB_UI_IMAGE_TAG .; cd ..
+echo "Finished building web_ui image"
+
+echo "Building rabbitMQ image..."
+cd ../src/rabbitmq; docker build -t $RABBITMQ_IMAGE_TAG .; cd ..
+echo "Finished building rabbitMQ image"
+
+echo "Building mongoDB image..."
+cd ../src/mongodb; docker build -t $MONGODB_IMAGE_TAG .; cd ..
+echo "Finished building mongoDB image"
