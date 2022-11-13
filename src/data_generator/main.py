a805a1d98a74f15da92c03ec9b9f45076b77f730
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2022-11-13 17:02:06 +0100

    data-generator fix, mongodb restart policy added

--- src/data_generator/main.py
+++ src/data_generator/main.py
@@ -1,14 +1,30 @@
 import random
 import time
+from datetime import datetime
+
+from faker import Faker
 
 from rabbitmq_client import RabbitMQClient
+
+fake = Faker()
 
 RabbitMQClient.create_connection()
 FREQUENCY = 7.5
 
 start_time = time.time()
+message_id = 1
+
+rand_date_begin = datetime.strptime('1/1/2021 1:00 AM', '%m/%d/%Y %I:%M %p')
+rand_date_end = datetime.strptime('1/1/2022 1:00 AM', '%m/%d/%Y %I:%M %p')
+
 while True:
-    RabbitMQClient.send_data_to_queue(queue_name="WildBoarQueue", payload=str(random.randrange(1, 10000000, 1)),body="id")
-    RabbitMQClient.send_data_to_queue(queue_name="WildBoarQueue", payload=str(random.randrange(5, 130, 5)),body="weights")
-    RabbitMQClient.send_data_to_queue(queue_name="WildBoarQueue", payload=str(random.choice([True, False])),body="occupancy")
+    random_payload = {
+        "id": message_id,
+        "weight": random.randrange(5, 130, 5),
+        "occupied": random.choice([True, False]),
+        "date": fake.date_time_between(start_date='-45d', end_date='now').strftime("%d/%m/%Y, %H:%M:%S")
+    }
+    print(str(random_payload))
+    RabbitMQClient.send_data_to_queue(queue_name="WildBoarQueue", payload=str(random_payload))
     time.sleep(5.0 - ((time.time() - start_time) % 5.0))
+    message_id += 1
