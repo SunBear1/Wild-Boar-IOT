a022aecb8e73b62d80aceab4fcd872a9d0101a12
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2022-11-15 15:14:42 +0100

    hardcoded docker image tags

--- deployment/build_images.sh
+++ deployment/build_images.sh
@@ -1,24 +1,24 @@
 echo "Building web_ui image..."
-cd ../src/web_ui; docker build -t $WEB_UI_IMAGE_TAG .;
+cd ../src/web_ui; docker build -t wildboar-iot-gym/frontend .;
 cd ..
 echo "Finished building web_ui image"
 
 echo "Building rabbitMQ image..."
-cd ../src/rabbitmq; docker build -t $RABBITMQ_IMAGE_TAG .;
+cd ../src/rabbitmq; docker build -t wildboar-iot-gym/message-broker .;
 cd ..
 echo "Finished building rabbitMQ image"
 
 echo "Building mongoDB image..."
-cd ../src/mongodb; docker build -t $MONGODB_IMAGE_TAG .;
+cd ../src/mongodb; docker build -t wildboar-iot-gym/database .;
 cd ..
 echo "Finished building mongoDB image"
 
 echo "Building data_generator image..."
-cd ../src/data_generator; docker build -t $DATA_GENERATOR_IMAGE_TAG .;
+cd ../src/data_generator; docker build -t wildboar-iot-gym/data-generator .;
 cd ..
 echo "Finished building data_generator image"
 
 echo "Building api image..."
-cd ../src/api/WebApi; docker build -t $API_IMAGE_TAG .;
+cd ../src/api/WebApi; docker build -t wildboar-iot-gym/backend .;
 cd ..
 echo "Finished building api image"
